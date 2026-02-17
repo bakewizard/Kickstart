e8d34d2096b490edec08f15e83ae3f937f5cb210
bakewizard <207716455+bakewizard@users.noreply.github.com>
2026-02-17 14:30:14 +0200

    refactoring...

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -6,73 +6,75 @@
 ?>
 <!doctype html>
 <html lang="<?= $config['App']['I18n']['currentLanguage'] ?>">
-    <head>
-        <title><?= $this->fetch('title') ?></title>
-        <?= $this->Html->charset() ?>
-        <?= $this->fetch('meta'); ?>
-        <?= $this->Html->meta('viewport', 'width=device-width, initial-scale=1') ?>
-        <?= $this->Html->meta('icon') ?>
-        <?= $this->Html->meta('csrfToken', $this->request->getAttribute('csrfToken')) ?>
 
-        <?= $this->Html->css('app') ?>
-        <?= $this->fetch('css') ?>
+<head>
+    <title><?= $this->fetch('title') ?></title>
+    <?= $this->Html->charset() ?>
+    <?= $this->fetch('meta'); ?>
+    <?= $this->Html->meta('viewport', 'width=device-width, initial-scale=1') ?>
+    <?= $this->Html->meta('icon') ?>
+    <?= $this->Html->meta('csrfToken', $this->request->getAttribute('csrfToken')) ?>
 
-        <?php foreach ($config['App']['I18n']['languages'] as $lang) : ?>
-            <?=
-            $this->Html->meta([
-                'rel' => 'alternate',
-                'link' => $this->Url->build(
-                    ['lang' => $lang === $config['App']['I18n']['defaultLanguage'] ? false : $lang] +
+    <?= $this->Html->css('app') ?>
+    <?= $this->fetch('css') ?>
+
+    <?php foreach ($config['App']['I18n']['languages'] as $lang) : ?>
+        <?=
+        $this->Html->meta([
+            'rel' => 'alternate',
+            'link' => $this->Url->build(
+                ['lang' => $lang === $config['App']['I18n']['defaultLanguage'] ? false : $lang] +
                     $this->request->getQueryParams() +
                     $this->request->getParam('pass'),
-                    ['fullBase' => true],
-                ),
-                'hreflang' => $lang,
-            ]);
-            ?>
-        <?php endforeach; ?>
-    </head>
-    <body>
-        <?= $this->element('layout/header') ?>
+                ['fullBase' => true],
+            ),
+            'hreflang' => $lang,
+        ]);
+        ?>
+    <?php endforeach; ?>
+</head>
 
-        <div class="container-lg">
-            <?= $this->Flash->render() ?>
-            <?= $this->element('layout/breadcrumbs') ?>
-            <div class="row">
-                <?php if ($this->fetch('sidebar-left')) : ?>
-                    <aside class="col-xl-3">
-                        <?= $this->fetch('sidebar-left') ?>
-                    </aside>
-                <?php endif; ?>
+<body>
+    <?= $this->element('layout/header') ?>
 
-                <?php if ($this->fetch('sidebar-left') && $this->fetch('sidebar-right')) : ?>
-                    <main class="col-xl-6">
-                        <?= $this->fetch('content') ?>
-                    </main>
-                <?php elseif ($this->fetch('sidebar-left') || $this->fetch('sidebar-right')) : ?>
-                    <main class="col-xl-9">
-                        <?= $this->fetch('content') ?>
-                    </main>
-                <?php else : ?>
-                    <main class="col-12">
-                        <?= $this->fetch('content') ?>
-                    </main>
-                <?php endif; ?>
+    <div class="container-lg">
+        <?= $this->Flash->render() ?>
+        <?= $this->element('layout/breadcrumbs') ?>
+        <?php
+        $sidebarLeft = $this->fetch('sidebar-left');
+        $sidebarRight = $this->fetch('sidebar-right');
+        $cols = match (true) {
+            $sidebarLeft && $sidebarRight => 6,
+            $sidebarLeft || $sidebarRight => 9,
+            default => 12
+        };
+        ?>
+        <div class="row">
+            <?php if ($sidebarLeft) : ?>
+                <aside class="col-xl-3">
+                    <?= $sidebarLeft ?>
+                </aside>
+            <?php endif; ?>
 
-                <?php if ($this->fetch('sidebar-right')) : ?>
-                    <aside class="col-xl-3">
-                        <?= $this->fetch('sidebar-right') ?>
-                    </aside>
-                <?php endif; ?>
-            </div>
+            <main class="col-xl-<?= $cols ?>">
+                <?= $this->fetch('content') ?>
+            </main>
+
+            <?php if ($sidebarRight) : ?>
+                <aside class="col-xl-3">
+                    <?= $sidebarRight ?>
+                </aside>
+            <?php endif; ?>
         </div>
+    </div>
 
-        <?= $this->element('layout/footer') ?>
+    <?= $this->element('layout/footer') ?>
 
-        <a id="scroll-up" href="#" class="btn btn-primary btn-lg scroll-up" role="button" title="Scroll up">
-            <span class="bi bi-chevron-up text-light"></span>
-        </a>
-        <?= $this->Html->script('app') ?>
-        <?= $this->fetch('script') ?>
-    </body>
+    <a id="scroll-up" href="#" class="btn btn-primary btn-lg scroll-up" role="button" title="Scroll up">
+        <span class="bi bi-chevron-up text-light"></span>
+    </a>
+    <?= $this->Html->script('app') ?>
+    <?= $this->fetch('script') ?>
+</body>
+
 </html>
